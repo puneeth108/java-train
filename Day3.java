9ae7f7e85ad393c60270438142a8f5321319b774
puneeth108 <reddypunith131@gmail.com>
2025-05-29 10:16:26 +0530

    fun

--- Day3.java
+++ Day3.java
@@ -1,5 +1,49 @@
-public class Day3{
+// import java.util.Scanner;
+// public class Day3 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for (int i = 0;i<n;i++){
+//             for(int j = i;j<n;j++){
+
+//                 System.out.print("*");
+
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+// import java.util.Scanner;
+// public class Day3 {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for (int i = 1;i<n*2;i++){
+//             int t =i>n ? 2*n-i :i;
+//             for(int j = 0;j<=t;j++){
+
+//                 System.out.print("*");
+
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+import java.util.Scanner;
+public class Day3 {
+
     public static void main(String[] args) {
-        System.out.println("h");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1;i<n;i++){
+            for(int j = 0;j<=;j++){
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
     }
 }
